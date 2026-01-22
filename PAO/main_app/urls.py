f8030148aa7154ad8465a313460a22d564c07b5e
zahidbalooshi <zahid.balooshi@gmail.com>
2026-01-22 21:28:20 +0300

    setting up dashboard

--- PAO/main_app/urls.py
+++ PAO/main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard')
 ]
