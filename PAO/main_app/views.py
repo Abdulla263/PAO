45fa33120f90eda2642a70a48834783b59effddb
Abdulla263 <abdullafawaz8@gmail.com>
2026-01-27 21:20:36 +0300

    Updated signup form

--- PAO/main_app/views.py
+++ PAO/main_app/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth import login
-
+from .forms import CustomUserCreationForm
 # Create your views here.
 def signup(request):
-    error_message=''
+    error_message = ''
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user= form.save()
-            login (request,user)
+            user = form.save()
+            login(request, user)
             return redirect('about')
         else:
             error_message = 'Invalid signup - try again'
-    form = UserCreationForm()
+    else:
+        form = CustomUserCreationForm()   
+
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
-
 
 
 
