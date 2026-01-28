8a513a75478335408b1c04aba4cf920a3ed5d33f
zahidbalooshi <zahid.balooshi@gmail.com>
2026-01-28 16:55:33 +0300

    added new models Quiz, Question, Answer

--- PAO/main_app/models.py
+++ PAO/main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 # Create your models here.
@@ -13,4 +14,28 @@ class Module(models.Model):
         return f"{self.title}"
 
 
+class Quiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.module.id}. {self.module.title} quiz by { self.user.username }"
+
+
+class Question(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    text = models.CharField()
+
+    def __str__(self):
+        return f"{self.text}"
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField()
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.text}"
 
