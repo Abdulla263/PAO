14905089d4415337e5c08b6b4ccd0598a237801f
Abdulla263 <abdullafawaz8@gmail.com>
2026-02-09 18:44:49 +0300

    Fixed autocreate profile

--- PAO/main_app/apps.py
+++ PAO/main_app/apps.py
@@ -1,13 +1,8 @@
 from django.apps import AppConfig
 
-
 class MainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'main_app'
 
-class UserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
-
-    # add this
     def ready(self):
-        import users.signals  
+        import main_app.signals
